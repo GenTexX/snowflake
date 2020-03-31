02851affe9dfe651fcd7171a2f77bb7b3026866c
Philip Schulz <Philip.Schulz.ps@gmail.com>
2020-03-31 11:05:02 +0200

    checkerboard pattern

--- sandbox/src/sandbox/sanbox2D.cpp
+++ sandbox/src/sandbox/sanbox2D.cpp
@@ -29,10 +29,22 @@ void Sandbox2D::onUpdate() {
 		this->m_CameraController.moveX(0.03f);
 
 	SF::Renderer::beginScene(this->m_CameraController.getCamera());
-	//SF::Renderer::drawQuad(glm::vec2(0.0f, 0.0f), glm::vec2(0.5f, 0.5f), 45.0f, glm::vec4(0.2f, 0.4f, 0.2f, 1.0f));
-	SF::Renderer::drawQuad(glm::vec2(1.0f, 2.0f), glm::vec2(0.2f, 1.5f), -10.0f, glm::vec4(0.8f, 0.4f, 0.2f, 1.0f));
-	//SF::Renderer::drawQuad(glm::vec2(-1.0f, 1.4f), glm::vec2(0.4f, 0.75f), 65.0f, glm::vec4(0.1f, 0.4f, 0.8f, 1.0f));
-	SF::Renderer::drawQuad(glm::vec2(1.0f, 0.0f), glm::vec2(3.0f, 1.0f), 30.0f, this->m_QuadTexture);
+	
+
+	bool colortoggle = false;
+	for (size_t i = 0; i < 27; i++) {
+		for (size_t j = 0; j < 21; j++) {
+			if (colortoggle) {
+				SF::Renderer::drawQuad(glm::vec2(-3.9 + 0.3f * i, -2.9 + j * 0.3f), glm::vec2(0.3f, 0.3f), 0.0f, glm::vec4(0.6f, 0.6f, 0.6f, 1.0f));
+			}
+			else {
+				SF::Renderer::drawQuad(glm::vec2(-3.9 + 0.3f * i, -2.9 + j * 0.3f), glm::vec2(0.3f, 0.3f), 0.0f, glm::vec4(0.9f, 0.9f, 0.9f, 1.0f));
+			}
+			colortoggle = !colortoggle;
+		}
+	}
+
+	SF::Renderer::drawQuad(glm::vec2(1.0f, 0.0f), glm::vec2(1.0f, 1.0f), 00.0f, this->m_QuadTexture);
 	SF::Renderer::endScene();
 	
 }
